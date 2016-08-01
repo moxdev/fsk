a4585d85f0a0011b10368f6fb7669cdcdbf1d51c
Mox Biggs <moxdev1@gmail.com>
2016-08-01 09:27:51 -0400

    done

--- residential-one-properties/js/location-map.js
+++ residential-one-properties/js/location-map.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		var js_file = document.createElement('script');
 		js_file.type = 'text/javascript';
-		js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBbb6_jT1FK3n7msQSis3VitMyULd6mwVA&callback=initialize&language=' + lang;
+		js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAte2D0BxrRUx5XfIO4yhvdPc8lyY_KRtA&callback=initialize&language=' + lang;
 		document.getElementsByTagName('body')[0].appendChild(js_file);
 	}
 });
